8d0773fe3d5979a454e43a452d665a09e1cf2421
Rohan Navale <devronaka@gmail.com>
2024-03-05 17:58:58 +0530

    Initial commit

---
+++ lib/categories.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView(gridDelegate: , children:[
+
+      ],)
+    );
+  }
+}
