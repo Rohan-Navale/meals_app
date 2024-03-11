025ce9036bcc721b28b324f014bd608704683654
Rohan Navale <devronaka@gmail.com>
2024-03-11 14:47:36 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: // Todo ...,
+      home: MaterialApp(),
     );
   }
 }
